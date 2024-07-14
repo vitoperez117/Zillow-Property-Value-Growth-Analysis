67f32d88a60c76aad615be21af1fc32d7c893399
Vito Perez <vitoperez117@gmail.com>
2024-07-14 10:21:41 -0700

    Update housing.py

--- housing.py
+++ housing.py
@@ -69,47 +69,48 @@ def percentiles(y):
         print(len(t))
 
 #Scatterplot for all neighborhoods
-plt.figure(figsize = (10, 6))  
+def plot_all_n():
+    plt.figure(figsize = (10, 6))  
+    
+    #Colorcode data points
+    conditions  = [ (y_all<=max) & (y_all>pct75), (y_all<=pct75) & (y_all>mean_all), (y_all<=mean_all) & (y_all>pct25), (y_all<=pct25) & (y_all>0), y_all<=0 ]
+    choices     = [ 'green', 'greenyellow', 'yellow', 'orange', 'red' ]
+    #choices     = [ 5,4,3,2,1 ]
+    colorall = np.select(conditions, choices, default=np.nan)
+    
+    #Plot Neighborhoods
+    plt.scatter( x_all, y_all, marker = 'o', s = 15, c = colorall) 
+    
+    #Plot Percentile Lines
+    minl = plt.axhline(y=min)
+    plt.text(len(x_all)-10, min, f'Min: {min}', fontsize = 10)
+    
+    pct25l = plt.axhline(y=pct25)
+    plt.text(len(x_all), 0, '25th Percentile', fontsize = 10)
+    
+    meanl = plt.axhline(y=mean_all)
+    plt.text(len(x_all), mean_all, 'MEAN | 50th Percentile', fontsize = 10)
+    
+    pct75l = plt.axhline(y=pct75)
+    plt.text(len(x_all), 150, '75th Percentile', fontsize = 10)
+    
+    maxl = plt.axhline(y=max)
+    plt.text(len(x_all), max, f'Max: {round(max,2)}', fontsize = 10)
+    
+    #Plot labels
+    plt.xticks([], rotation = 90)
+    plt.title("All Neighborhoods based on 5 Year Rate of Change")
+    plt.ylabel("Growth Rate %")
+    plt.xlabel("Neighborhoods")
+    
+    plt.tight_layout()
+    #plt.savefig('../2024/Output Plots/All Neighborhoods 5 year ROC')
+    plt.show()
 
-#Colorcode data points
-conditions  = [ (y_all<=max) & (y_all>pct75), (y_all<=pct75) & (y_all>mean_all), (y_all<=mean_all) & (y_all>pct25), (y_all<=pct25) & (y_all>0), y_all<=0 ]
-choices     = [ 'green', 'greenyellow', 'yellow', 'orange', 'red' ]
-#choices     = [ 5,4,3,2,1 ]
-colorall = np.select(conditions, choices, default=np.nan)
-
-#Plot Neighborhoods
-plt.scatter( x_all, y_all, marker = 'o', s = 15, c = colorall) 
-
-#Plot Percentile Lines
-minl = plt.axhline(y=min)
-plt.text(len(x_all)-10, min, f'Min: {min}', fontsize = 10)
-
-pct25l = plt.axhline(y=pct25)
-plt.text(len(x_all), 0, '25th Percentile', fontsize = 10)
-
-meanl = plt.axhline(y=mean_all)
-plt.text(len(x_all), mean_all, 'MEAN | 50th Percentile', fontsize = 10)
-
-pct75l = plt.axhline(y=pct75)
-plt.text(len(x_all), 150, '75th Percentile', fontsize = 10)
-
-maxl = plt.axhline(y=max)
-plt.text(len(x_all), max, f'Max: {round(max,2)}', fontsize = 10)
-
-#Plot labels
-plt.xticks([], rotation = 90)
-plt.title("All Neighborhoods based on 5 Year Rate of Change")
-plt.ylabel("Growth Rate %")
-plt.xlabel("Neighborhoods")
-
-plt.tight_layout()
-#plt.savefig('../2024/Output Plots/All Neighborhoods 5 year ROC')
-plt.show()
-
+plot_all_n()
 
 #Data frame for top 50
 plotdf=alldf.head(50)
-plotdf['%']=plotdf['Rate of Change 5year'].apply(lambda x: x*100)
 
 #Assign x and y axes
 x_axis=plotdf['RegionName']
@@ -123,32 +124,34 @@ mean=y_axis.mean()
 mean
 
 
-#Scatterplot for top 50
-plt.figure(figsize = (7, 9))  
+#Top 50 plot
 
-#Color code data points
-conditions  = [ y_axis >= mean, (y_axis<mean) & (y_axis>median), y_axis <= median ]
-choices     = [ 'green', 'orange', 'red' ]
+def plot_top_50():
+    plt.figure(figsize = (7, 9))  
     
-color = np.select(conditions, choices, default=np.nan)
+    #Color code data points
+    conditions  = [ y_axis >= mean, (y_axis<mean) & (y_axis>median), y_axis <= median ]
+    choices     = [ 'blue', 'orange', 'red' ]
+        
+    color = np.select(conditions, choices, default=np.nan)
+    
+    #Plot Neighborhoods
+    plt.scatter( y_axis, x_axis, marker = 'o', s = 15, color = color) 
+    
+    #Plot Median Line
+    m1 = plt.axvline(x=np.median(y_axis), ls=':')
+    plt.text(median-6.5, -2, str(round(median,2)), fontsize = 10)
+    
+    #Plot Mean Line
+    m2 = plt.axvline(x=np.mean(y_axis))
+    plt.text(mean, -2, str(round(mean,2)), fontsize = 10)
+    
+    plt.legend([m1,m2],['Median', 'Mean'],loc = "upper right",fontsize = "medium",fancybox = True)
+    plt.title("Top 50 Neighborhoods based on 5 Year Rate of Change")
+    plt.ylabel("Neighborhoods")
+    plt.yticks(fontfamily='sans-serif', fontsize='x-small')
+    plt.xlabel("Growth Rate %")
+    
+    plt.show()
 
-#Plot Neighborhoods
-plt.scatter( y_axis, x_axis, marker = 'o', s = 15, color = color) 
-
-#Plot Median Line
-m1 = plt.axvline(x=np.median(y_axis), ls=':')
-plt.text(median-6.5, -2, str(round(median,2)), fontsize = 10)
-
-#Plot Mean Line
-#m2 = plt.axvline(x=np.mean(y_axis))
-#plt.text(mean, -2, str(round(mean,2)), fontsize = 10)
-
-plt.legend([m1],['Median'],loc = "upper right",fontsize = "medium",fancybox = True)
-plt.title("Top 50 Neighborhoods based on 5 Year Rate of Change")
-plt.ylabel("Neighborhoods")
-plt.yticks(fontfamily='sans-serif', fontsize='x-small')
-plt.xlabel("Growth Rate %")
-
-plt.tight_layout()
-#plt.savefig('../2024/Output Plots/Top 50 Neighborhoods scatterplot')
-plt.show()
+plot_top_50()
